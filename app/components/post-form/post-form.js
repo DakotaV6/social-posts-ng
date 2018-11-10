14b9210ef67dcee315f435a9ae76166bf1754e5c
Dakota <dakota.vradenburg@gmail.com>
2018-11-10 14:48:28 -0500

    Added expandOnFocus directive and added to form inputs.

--- app/components/post-form/post-form.js
+++ app/components/post-form/post-form.js
@@ -4,9 +4,9 @@ const postForm = {
     template: `
     <form ng-submit="$ctrl.onSubmit({newPost: $ctrl.post});" class="border">
     <label for="post-title">Title</label>
-    <input type="text" name="post-title" ng-model="$ctrl.post.subject" placeholder="Post title" class="border">
+    <input expand-on-focus type="text" name="post-title" ng-model="$ctrl.post.subject" placeholder="Post title" class="border">
     <label for="post-body">Post Content</label>
-    <textarea name="post-body" ng-model="$ctrl.post.body" class="border"></textarea>
+    <textarea expand-on-focus name="post-body" ng-model="$ctrl.post.body" class="border"></textarea>
     <button id="add-btn" class="border">Add Post</button>
     `,
     bindings: {
---
+++ app/directive/expandOnFocus.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function expandOnFocus() {
+    return {
+        restrict: "A",
+        link: function ($scope, $element, $attrs) {
+            console.log($element);
+            $element.on("focus", () => {
+                if ($element[0].tagName === "INPUT") {
+                    console.log("FOCUSED");
+                    $element.css("height", "70px");
+                }
+                if ($element[0].tagName === "TEXTAREA") {
+                    $element[0].parentElement.style.height = "400px";
+                    $element.css("height", "60%");
+                }
+            });
+            $element.on("blur", () => {
+                if ($element[0].tagName === "INPUT") {
+                    $element.css("height", "auto");
+                }
+                if ($element[0].tagName === "TEXTAREA") {
+                    $element[0].parentElement.style.height = "300px";
+                    $element.css("height", "50%");
+                }
+            });
+        }
+    }
+
+}
+
+angular
+    .module("App")
+    .directive("expandOnFocus", expandOnFocus)
